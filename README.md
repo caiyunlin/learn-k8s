2500221d55b959783ebcb3d63f1cd6307782c751
Calvin Cai <cylin2000@163.com>
2025-01-06 14:55:18 +0800

    update hello-world

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # learn-k8s
 Learn Kubernetes
+
+
+## Test on Play with K8S env 
+- https://labs.play-with-k8s.com/
+
+### Init
+```bash
+$scriptPath = "https://raw.githubusercontent.com/caiyunlin/learn-k8s/refs/heads/main/src"
+```
+
+### Deploy hello-world nginx pod
+```bash
+kubectl apply -f "$scriptPath/01_pod.yaml"
+kubectl get pods # you should see the 'nginx-pod' running
+```
--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,10 @@
 
 # Set the current directory to the directory of the script
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+# $scriptPath = "c:/work/github/learn-k8s/src"
 
-$scriptPath = ""
+# Play with K8S env : 
+$scriptPath = "https://raw.githubusercontent.com/caiyunlin/learn-k8s/refs/heads/main/src"
 
-kubectl apply -f $scriptPath\src\01_pod.yaml
+kubectl apply -f "$scriptPath/01_pod.yaml"
+kubectl get pods # you should see the 'nginx-pod' running
