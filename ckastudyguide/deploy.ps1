0380838b36eee832b2b343ca4973fb4880364419
Calvin Cai <cylin2000@163.com>
2025-03-17 09:10:49 +0800

    update lab test script

--- ckastudyguide/deploy.ps1
+++ ckastudyguide/deploy.ps1
@@ -5,9 +5,18 @@ kubectl get pods
 
 kubectl delete pod manually-nginx-pod
 
+
+## Create Deployment via script
+kubectl create deployment nginx-deployment --image=nginx:1.19.5 --replicas=6
+kubectl describe deployment nginx-deployment # check the deployment details, including strategy, replicas, etc.
+kubectl update deployment nginx-deployment 
+
+
 # Create Deployments 
 kubectl apply -f .\0202-nginx-deployment.yaml
 kubectl get deployments
+
+
 
 # Scale Deployment
 kubectl scale deployment nginx-deployment --replicas=3
@@ -25,5 +34,13 @@ kubectl apply -f .\0204-stateful-deployment.yaml
 
 
 ## Config Map and Secrets
-kubectl create configmap cm1 --from-literal=mykey=myvalue
-kubectl get configmap cm1 -o yaml
+kubectl create configmap mycm --from-literal=owner=calvin
+kubectl get configmap mycm -o yaml
+kubectl describe configmap mycm
+kubectl delete configmap mycm
+kubectl apply -f .\0208-exercise2-configmap.yaml
+
+kubectl create secret generic mysecret --from-literal=dbuser="MyDatabaseUser" --from-literal=dbpassword="MyDatabasePassword" 
+kubectl apply -f .\0209-exercise3-secret.yaml
+kubectl logs nginx-secret | grep db
+kubectl get secret mysecret -o yaml
